99bf73704739dd0d0d801336360a303f27a791fe
bem130 <haruki070617@gmail.com>
2022-11-20 20:18:34 +0900

    change a little

--- data/3d.js
+++ data/3d.js
@@ -70,7 +70,7 @@ class tdDRAW {
                             if (zbuf[idex]>p) {
                                 zbuf[idex] = p;
                                 let index = idex*4;
-                                light = (Math.max(angl,angl*0.1)*0.9+0.3)*(1000/(p**2+1000)); // 面と平行光源の角度
+                                light = (Math.max(angl,angl*0.1)*0.9+0.3)*(10000/(p**2+1000)); // 面と平行光源の角度
                                 iarr[index+0] = t[3][0]*light; // 赤の描画
                                 iarr[index+1] = t[3][1]*light; // 緑の描画
                                 iarr[index+2] = t[3][2]*light; // 青の描画
