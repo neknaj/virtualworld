4d27c415ec1006e275d68499f83ca13479bec4b5
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2022-12-01 22:05:41 +0900

    ref: server.js

--- server.js
+++ server.js
@@ -1,61 +1,44 @@
-const port = 8080;
 const http = require("http");
 const fs = require('fs');
+
+const port = 8080;
+
 const server = http.createServer(server_request);
 server.listen(port);
 const socket = require('socket.io')(server);
 console.log(`The server has started and is listening on port number: ${port}`);
 
 function server_request(req, res) {
-    let head = {
-        "Content-Type": "text/html",
-    }
-    let rescode = 200;
+    const head = { "Content-Type": "text/html" };
+    const rescode = 200;
 
     let resd = "";
     let nfflag = false;
-    switch (req.url.split(".")[0]) {
-      case "/index": // title page
-      head["Location"] = "/";rescode = 302;
-      break;
-      case "/": // title page
-      try {
-          resd += fs.readFileSync("data/top.html", 'utf8');
-      } catch (e) {nfflag=true;}
-      break;
-      case "/world": // main page
-      try {
-          resd += fs.readFileSync("data/world.html", 'utf8');
-      } catch (e) {nfflag=true;}
-      break;
-      case "/3d": // 3d library
-        try {
-            resd += fs.readFileSync("data/3d.js", 'utf8');
-            head["Content-Type"] = "application/javascript";
-        } catch (e) {nfflag=true;}
-      break;
-      case "/style": // style
-        try {
-            resd += fs.readFileSync("data/style.css", 'utf8');
-            head["Content-Type"] = "text/css";
-        } catch (e) {nfflag=true;}
-      break;
-      case "/getworld": // get world data
+    console.log(req.url)
+    if(req.url.startsWith("/index")){ // title page head["Location"] = "/";
+      rescode = 302;
+    } else if(req.url.startsWith("/getworld")){ // get world data 
         resd += "comming soon";
         resd += "<a href=\"/\">top</a>";
-      break;
-      default: // not found
-        nfflag = true;
-      break;
+    } else {
+      try {
+        const path = {
+          "/": "top.html",
+          "/world": "world.html"
+        }[req.url] || req.url;
+        resd += fs.readFileSync(`data${path}`, 'utf8');
+        head["Content-Type"] = { // Content-Typeを設定
+          js:  "application/javascript",
+          css: "text/css"
+        }[req.url.split(".").reverse()[0]] || "text/html";
+      } catch (e) {
+        // 404 not found
+        rescode = 404;
+        resd += `<h1> Not found</h1>
+                <p>url: ${req.url}</p>`
+      }
     }
-    if (nfflag) {
-      rescode = 404;
-      resd += "<h1>Not found</h1>";
-      resd += "<p>";
-      resd += "url: ";
-      resd += req.url;
-      resd += "</p>";
-    }
+    
     res.writeHead(rescode, head);
     res.end(resd);
 }
